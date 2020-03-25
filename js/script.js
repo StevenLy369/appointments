a12acfb7712be12087424a32f1deb35cd5a26613
Steven Ly <1stevenly92@gmail.com>
2020-03-25 13:59:46 -0700

    was able to store and console log

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var name;
+
+
+
+$(document).ready(function(){
+
+    $("form#info").submit(function(event){
+        event.preventDefault();
+
+        var name =($("#name").val());
+        
+    console.log(name);
+
+
+    })
+
+
+
+
+
+
+
+
+})
