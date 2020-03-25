9d94ab76f6f28143866adb984ae07be65e9e6d83
Steven Ly <1stevenly92@gmail.com>
2020-03-25 15:11:58 -0700

    added modal

--- js/script.js
+++ js/script.js
@@ -8,9 +8,18 @@ $(document).ready(function(){
         event.preventDefault();
 
         var name =($("#name").val());
+        var descrip =($("#descrip").val());
+        var date =($("#date").val());
+        var start=($("#start").val());
         
-    console.log(name);
+        var appointment = (" Name:  " + name + "  Description  " + descrip + "  Date  " + date + "  Start time  " + start );
 
+
+        $("#output1").text( "Here is your appointment info: " + appointment);
+        // $("#output").text("Here is your appointment info: " + appointment );
+        
+        
+    
 
     })
 
